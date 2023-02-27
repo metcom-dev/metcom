7b9d485c9d3fc4ec2cc0f19f7767569239ea7946
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2023-02-27 14:32:22 -0500

    [UPD] custom_project: Cambio de vista de Mano de Obra

--- custom_project/models/project.py
+++ custom_project/models/project.py
@@ -7,7 +7,6 @@ class Project(models.Model):
     _inherit = 'project.project'
 
     requirement_ids = fields.One2many(string='Requerimientos', comodel_name='purchase.preorder', inverse_name='project_id', copy=True, readonly=True)
-    labor_ids = fields.One2many(string='Mano de Obra', comodel_name='project.labor', inverse_name='project_id', copy=True)
     purchase_attachs_ids = fields.One2many(
         string='Ordenes de Compra',
         comodel_name='project.attachment',
@@ -55,21 +54,6 @@ class Project(models.Model):
             vals['name'] = sequence + vals['name']
         res = super(Project, self).create(vals)
         return res
-
-class ProjectLabor(models.Model):
-    _name = 'project.labor'
-    _description = 'Lineas de Mano de Obra de Proyecto'
-
-    project_id = fields.Many2one(string='Proyecto', comodel_name='project.project', ondelete='cascade')
-    employee_id = fields.Many2one(string="Empleado", comodel_name='hr.employee', ondelete='restrict', required=True)
-    employee_vat = fields.Char(string="DNI")
-    hours = fields.Float(string="Horas Dedicadas", required=True)
-
-    @api.onchange('employee_id')
-    def _onchange_employee_id(self):
-        if not self.employee_id:
-            return
-        self.employee_vat = self.employee_id.identification_id
 
 class ProjectAttachment(models.Model):
     _name = 'project.attachment'
