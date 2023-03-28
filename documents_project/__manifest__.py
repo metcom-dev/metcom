475b3eee905fdd0591da1d882b1907654a8289c8
Raul122-dev <victor.huaycho@tecsup.edu.pe>
2023-03-28 15:46:44 -0500

    [IMP] documents_project: modulo para el listado de archivos por proyectos

---
+++ documents_project/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Files for Projects',
+    'description': 'Este módulo lista los archivos por proyecto y almacen.',
+    'category': 'Project Management',
+    'version': '1.0',
+    'author': 'Conflux',
+    'depends': ['base', 'project'],
+    'data': [
+        'views/files_view.xml',
+        'security/ir.model.access.csv',
+        'security/security.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
