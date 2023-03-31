de199455446578cbaa2e23f888bad6206fe2507b
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2023-03-31 18:12:22 -0500

    [UPD] custom_documents_project: Implementacion de funcion para descargar adjuntos desde proyectos

--- custom_documents_project/models/documents_document.py
+++ custom_documents_project/models/documents_document.py
@@ -6,3 +6,10 @@ log = logging.getLogger(__name__)
 class DocumentsDocument(models.Model):
     _inherit = 'documents.document'
 
+    def download_file_metcom(self):
+        for rec in self:
+            return {
+                'type': 'ir.actions.act_url',
+                'url': '/web/content/%s?download=true' % (rec.attachment_id.id),
+                'target': 'new',
+            }
