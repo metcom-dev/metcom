0e6eee668382a1f6be1ae6fdc47f77cf758866da
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2023-04-14 17:47:56 -0500

    [FIX] custom_order_print: Quitando módulo

--- custom_order_print/models/purchase_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, api, models, _
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-    carrier_id = fields.Many2one('res.partner', string='Transportista', domain=[('is_carrier', '=', True)])
