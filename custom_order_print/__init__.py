bb33de547764d66103d7a2ab3afe503d8d1f403c
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2023-04-20 16:56:46 -0500

    [FIX] custom_order_print: Regularizando módulo

--- custom_order_print/__init__.py
+++ custom_order_print/__init__.py
@@ -1,23 +1,2 @@
-# -*- coding: utf-8 -*-
-{
-    'name' : 'Perzonalización de Impresión de Ordenes de Compra',
-    'version' : '16.0.1',
-    'summary': 'Perzonalización de Impresión de Ordenes de Compra MetCom',
-    'sequence': 10,
-    'description': """
-        Perzonalización de Impreción de Ordenes de Compra MetCom
-    """,
-    'category': 'Purchase',
-    'license': 'OPL-1',
-    'website': 'https://conflux.pe',
-    'depends' : ['purchase', 'custom_contact'],
-    'data': [
-        #
-        "views/purchase_order.xml",
-        "views/purchase_order_template_inherit.xml"
-    ],
-    'qweb': [],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-}
+from . import models
+from . import reports
