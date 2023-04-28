87bbdeff9e7bd4097496b47f2fef454a71c714f7
Raul122-dev <victor.huaycho@tecsup.edu.pe>
2023-04-28 16:30:45 -0500

    [IMP] custom_order_print: posibilidad de seleccionar el banco a usar en la orden

--- custom_order_print/models/purchase_order.py
+++ custom_order_print/models/purchase_order.py
@@ -1,6 +1,44 @@
 from odoo import fields, api, models, _
 
+import logging
+log = logging.getLogger(__name__)
+
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     carrier_id = fields.Many2one('res.partner', string='Transportista', domain=[('is_carrier', '=', True)])
+    bank_type = fields.Many2one('res.bank', string='Banco')
+    partner_banks = fields.One2many(related='partner_id.bank_ids', readonly=True)
+    allowed_bank_type_ids = fields.Many2many(
+        'res.bank', compute='_compute_allowed_bank_type_ids'
+    )
+
+    @api.depends('partner_id')
+    def _compute_allowed_bank_type_ids(self):
+        for order in self:
+            bank_type_ids = []
+            for bank in order.partner_id.bank_ids:
+                if bank.bank_id.id not in bank_type_ids:
+                    bank_type_ids.append(bank.bank_id.id)
+            if not bank_type_ids:
+                order.bank_type = False
+            order.allowed_bank_type_ids = bank_type_ids
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            self.bank_type = False
+            bank_type_ids = []
+            if self.partner_id.bank_ids: 
+                for bank in self.partner_id.bank_ids:
+                    if bank.bank_id.id not in bank_type_ids:
+                        bank_type_ids.append(bank.bank_id.id)
+                return {'domain': {'bank_type': [('id', 'in', bank_type_ids)]}}
+            else:
+                self.bank_type = False 
+                return {'domain': {'bank_type': []}}
+        else:
+            self.bank_type = False
+            return {'domain': {'bank_type': []}}
+        
+    
