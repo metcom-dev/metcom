3afd2283e4aed12563df8a8cc6b138eb7f57e53c
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2023-05-02 15:42:19 -0500

    [FIX] custom_purchase_order: Reparando herencia de modelo

--- custom_purchase_order/models/purchase_order.py
+++ custom_purchase_order/models/purchase_order.py
@@ -3,5 +3,5 @@ from odoo import models, fields, api
 import logging
 log = logging.getLogger(__name__)
 
-class PrePurchase(models.Model):
-    _inherit = 'purchase.preorder'
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
