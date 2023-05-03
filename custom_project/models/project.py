8ddff6fc112215d3689a2641321d966a81b60d10
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2023-05-03 11:09:33 -0500

    [UPD] custom_project: Agregando tracking a cuenta analitica de proyecto

--- custom_project/models/project.py
+++ custom_project/models/project.py
@@ -38,7 +38,7 @@ class Project(models.Model):
         help="Analytic account to which this project, its tasks and its timesheets are linked. \n"
             "Track the costs and revenues of your project by setting this analytic account on your related documents (e.g. sales orders, invoices, purchase orders, vendor bills, expenses etc.).\n"
             "This analytic account can be changed on each task individually if necessary.\n"
-            "An analytic account is required in order to use timesheets.", track_visibility='onchange')
+            "An analytic account is required in order to use timesheets.", tracking=True)
     
     _sql_constraints = [
         ('name_uniq', 'unique(name)', 'El nombre del proyecto debe ser unico.'),
