e52828669724410d96e5c66d1c3a78f767350984
edwardmf94 <edward@resit.cl>
2023-05-17 18:06:49 -0500

    [FIX] project_restrict_group

--- project_restrict_group/models/project.py
+++ project_restrict_group/models/project.py
@@ -6,22 +6,26 @@ class Project(models.Model):
     restrict_group_id = fields.Many2one('res.groups', string='Restrict Group')
 
     def action_create_group(self):
-        if not self.restrict_group_id:
-            group = self.env['res.groups'].create({
-                'name': 'Project %s ' % self.name,
-                'users': [(6, 0, [self.user_id.id])],
-                'rule_groups':[(0, 0, {
-                    'name':'Project %s ' % self.name,
-                    'model_id':self.env['ir.model'].search([('model','=','project.project')]).id,
-                    'domain_force':"[('id','=',%s)]" % self.id,
-                    'perm_read':True,
-                    'perm_write':True,
-                    'perm_create':True,
-                    'perm_unlink':True,
-                })]
-            })
-            self.restrict_group_id = group.id
+        for rec in self:
+            if not rec.restrict_group_id:
+                group = self.env['res.groups'].create({
+                    'name': 'Project %s ' % self.name,
+                    'users': [(6, 0, [self.user_id.id])],
+                    'rule_groups':[(0, 0, {
+                        'name':'Project %s ' % self.name,
+                        'model_id':self.env['ir.model'].search([('model','=','project.project')]).id,
+                        'domain_force':"[('id','=',%s)]" % self.id,
+                        'perm_read':True,
+                        'perm_write':True,
+                        'perm_create':True,
+                        'perm_unlink':True,
+                    })]
+                })
+                rec.restrict_group_id = group.id
 
-    def create(self, vals):
-        res = super(Project, self).create(vals)
-        res.action_create_group()
+    @api.model_create_multi
+    def create(self, vals_list):
+        self = self.with_context(mail_create_nosubscribe=True)
+        projects = super().create(vals_list)
+        projects.action_create_group()
+        return projects
