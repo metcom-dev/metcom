f4da7f2eff9b01161d118dad0647e16457c711b1
edwardmf94 <edward@resit.cl>
2023-05-18 18:00:05 -0500

    [FIX] project_restrict_group: creacion de grupo de usuario al editar tipo de proyecto

--- project_restrict_group/models/project.py
+++ project_restrict_group/models/project.py
@@ -4,10 +4,11 @@ class Project(models.Model):
     _inherit = 'project.project'
 
     restrict_group_id = fields.Many2one('res.groups', string='Restrict Group')
+    privacy_visibility = fields.Selection(selection_add=[('user_group', 'User Group')])
 
     def action_create_group(self):
         for rec in self:
-            if not rec.restrict_group_id:
+            if not rec.restrict_group_id and rec.privacy_visibility == 'user_group':
                 group = self.env['res.groups'].create({
                     'name': 'Project %s ' % self.name,
                     'users': [(6, 0, [self.user_id.id])],
@@ -29,3 +30,8 @@ class Project(models.Model):
         projects = super().create(vals_list)
         projects.action_create_group()
         return projects
+    
+    def write(self, vals):
+        res = super(Project, self).write(vals)
+        res.action_create_group()
+        return res
