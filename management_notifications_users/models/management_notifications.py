da58043e748e0ca17a02f968f2667aec637af575
Raul122-dev <victor.huaycho@tecsup.edu.pe>
2023-07-17 10:09:33 -0500

    [UDP] management_notifications_users: cambio de email_to para usar los correos de notificaciones activas

--- management_notifications_users/models/management_notifications.py
+++ management_notifications_users/models/management_notifications.py
@@ -56,7 +56,7 @@ class ManagementNotifications(models.Model):
             mail_id = self.env['mail.mail'].create({
                 'subject': subject,
                 'body_html': mail_body,
-                'email_to': 'victor.huaycho@tecsup.edu.pe,vhuaycho@conflux.pe',
+                'email_to': email_to,
                 'auto_delete': True,
                 'message_type': 'email',
             })
