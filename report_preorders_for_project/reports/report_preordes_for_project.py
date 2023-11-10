f68a138202b305d32b45a27cf8ac85568ab3b439
Raul122-dev <victor.huacho@tecsup.edu.pe>
2023-11-10 08:16:47 -0500

    [UPD] report_preorders_for_project: se actualiza el reporte de preordenes para filtrar por fecha, se agrega campo de proyecto

--- report_preorders_for_project/reports/report_preordes_for_project.py
+++ report_preorders_for_project/reports/report_preordes_for_project.py
@@ -10,7 +10,9 @@ class ReportPreorderProject(models.AbstractModel):
 
     def generate_xlsx_report(self, workbook, data, lines):
         sheet = workbook.add_worksheet('Reporte de pre-ordenes por proyecto')
-        data = self.get_data(data['project_id'])
+        from_date = data['from_date']
+        to_date = data['to_date']
+        data = self.get_data(from_date + " 00:00:00", to_date + " 23:59:59")
         user_lang = self.env.user.lang
 
         formats = {
@@ -23,86 +25,89 @@ class ReportPreorderProject(models.AbstractModel):
         }
         sheet.merge_range('A1:G1', 'REPORTE DE PRE-ORDENES POR PROYECTO', formats['bold_center'])
 
-        sheet.merge_range('A2:B2', 'Proyecto: ', formats['bold_center'])
-        if len(data) > 0:
-            sheet.merge_range('C2:G2', data[0].get('name_project').get(user_lang, ''), formats['normal_left'])
+        sheet.write(1, 1, 'Desde:', formats['bold_right'])
+        sheet.write(1, 2, from_date, formats['date_format'])
+        sheet.write(2, 1, 'Hasta:', formats['bold_right'])
+        sheet.write(2, 2, to_date, formats['date_format'])
 
         sheet.set_column('A:A', 5)
         sheet.set_column('B:B', 15)
         sheet.set_column('C:C', 15)
-        sheet.set_column('D:D', 10)
-        sheet.set_column('E:E', 60)
-        sheet.set_column('F:F', 10)
+        sheet.set_column('D:D', 25)
+        sheet.set_column('E:E', 10)
+        sheet.set_column('F:F', 60)
         sheet.set_column('G:G', 10)
         sheet.set_column('H:H', 10)
         sheet.set_column('I:I', 10)
         sheet.set_column('J:J', 10)
         sheet.set_column('K:K', 10)
-        sheet.set_column('L:L', 30)
-        sheet.set_column('M:M', 15)
-        sheet.set_column('N:N', 10)
-        sheet.set_column('O:O', 15)
-        sheet.set_column('P:P', 20)
+        sheet.set_column('L:L', 10)
+        sheet.set_column('M:M', 30)
+        sheet.set_column('N:N', 15)
+        sheet.set_column('O:O', 10)
+        sheet.set_column('P:P', 15)
+        sheet.set_column('Q:Q', 20)
+        sheet.set_column('R:R', 15)
 
         
-        row_header= 3
+        row_header= 4
        
         sheet.write(row_header, 1, 'Nro de Pre-Orden', formats['bold_center'])
         sheet.write(row_header, 2, 'Fecha de Pre-Orden', formats['bold_center'])
-        # sheet.write(row_header, 3, 'Proyecto', formats['bold_center'])
-        sheet.write(row_header, 3, 'Codigo', formats['bold_center'])
-        sheet.write(row_header, 4, 'Material', formats['bold_center'])
-        sheet.write(row_header, 5, 'Unidad', formats['bold_center'])
-        sheet.write(row_header, 6, 'Cantidad pedida', formats['bold_center'])
-        sheet.write(row_header, 7, 'Cantidad atendida', formats['bold_center'])
-        sheet.write(row_header, 8, 'Materiales con stock', formats['bold_center'])
-        sheet.write(row_header, 9, 'Materiales comprados', formats['bold_center'])
-        sheet.write(row_header, 10, 'Orden de Compra', formats['bold_center'])
-        sheet.write(row_header, 11, 'Proveedor', formats['bold_center'])
-        sheet.write(row_header, 12, 'Precio Unitario sin IGV', formats['bold_center'])
-        sheet.write(row_header, 13, 'Monto sin IGV', formats['bold_center'])
-        sheet.write(row_header, 14, 'Moneda', formats['bold_center'])
-        sheet.write(row_header, 15, 'Fecha de Compra', formats['bold_center'])
-        sheet.write(row_header, 16, 'Usuario Emisor', formats['bold_center'])
+        sheet.write(row_header, 3, 'Proyecto', formats['bold_center'])
+        sheet.write(row_header, 4, 'Codigo', formats['bold_center'])
+        sheet.write(row_header, 5, 'Material', formats['bold_center'])
+        sheet.write(row_header, 6, 'Unidad', formats['bold_center'])
+        sheet.write(row_header, 7, 'Cantidad pedida', formats['bold_center'])
+        sheet.write(row_header, 8, 'Cantidad atendida', formats['bold_center'])
+        sheet.write(row_header, 9, 'Materiales con stock', formats['bold_center'])
+        sheet.write(row_header, 10, 'Materiales comprados', formats['bold_center'])
+        sheet.write(row_header, 11, 'Orden de Compra', formats['bold_center'])
+        sheet.write(row_header, 12, 'Proveedor', formats['bold_center'])
+        sheet.write(row_header, 13, 'Precio Unitario sin IGV', formats['bold_center'])
+        sheet.write(row_header, 14, 'Monto sin IGV', formats['bold_center'])
+        sheet.write(row_header, 15, 'Moneda', formats['bold_center'])
+        sheet.write(row_header, 16, 'Fecha de Compra', formats['bold_center'])
+        sheet.write(row_header, 17, 'Usuario Emisor', formats['bold_center'])
 
-        row = 4
+        row = 5
         if len(data) > 0:
             for item in data:
                 sheet.write(row, 1, item['name'], formats['normal_center'])
                 sheet.write(row, 2, item['date_order'], formats['date_format'])
-                # sheet.write(row, 3, item.get('name_project').get(user_lang, ''), formats['normal_left'])
-                sheet.write(row, 3, item['codigo_material'], formats['normal_center'])
+                sheet.write(row, 3, item.get('name_project').get(user_lang, ''), formats['normal_left'])
+                sheet.write(row, 4, item['codigo_material'], formats['normal_center'])
 
                 nombre_material = item.get('nombre_material')
                 unidadmedida = item.get('unidadmedida')
                 if nombre_material:
-                    sheet.write(row, 4, nombre_material.get(user_lang, ''), formats['normal_left'])
+                    sheet.write(row, 5, nombre_material.get(user_lang, ''), formats['normal_left'])
                 else:
-                    sheet.write(row, 4, '', formats['normal_left'])
+                    sheet.write(row, 5, '', formats['normal_left'])
 
                 if unidadmedida:
-                    sheet.write(row, 5, unidadmedida.get(user_lang, ''), formats['normal_center'])
+                    sheet.write(row, 6, unidadmedida.get(user_lang, ''), formats['normal_center'])
                 else:
-                    sheet.write(row, 5, '', formats['normal_center'])
+                    sheet.write(row, 6, '', formats['normal_center'])
 
-                sheet.write(row, 6, item['cantidad_pedida'], formats['normal_center'])
-                sheet.write(row, 7, item['cantidad_atendida'], formats['normal_center'])
-                sheet.write(row, 8, item['materiale_stock'], formats['normal_center'])
+                sheet.write(row, 7, item['cantidad_pedida'], formats['normal_center'])
+                sheet.write(row, 8, item['cantidad_atendida'], formats['normal_center'])
+                sheet.write(row, 9, item['materiale_stock'], formats['normal_center'])
                 
                 cantidad_pedida = item.get('cantidad_pedida') or 0
                 materiale_stock = item.get('materiale_stock') or 0
 
                 materiales_comprados = int(cantidad_pedida) - int(materiale_stock)
 
-                sheet.write(row, 9, materiales_comprados if materiales_comprados > 0 else 0, formats['normal_center'])
+                sheet.write(row, 10, materiales_comprados if materiales_comprados > 0 else 0, formats['normal_center'])
 
-                sheet.write(row, 10, item['name_purchase_order'], formats['normal_center'])
-                sheet.write(row, 11, item['name_proveedor'], formats['normal_left'])
-                sheet.write(row, 12, item['precio_unitario'], formats['normal_center'])
-                sheet.write(row, 13, item['monto_sin_igv'], formats['normal_center'])
-                sheet.write(row, 14, item['tipo_moneda'], formats['normal_center'])
-                sheet.write(row, 15, item['fecha_compra'], formats['date_format'])
-                sheet.write(row, 16, item['usuario_emisor'], formats['normal_center'])
+                sheet.write(row, 11, item['name_purchase_order'], formats['normal_center'])
+                sheet.write(row, 12, item['name_proveedor'], formats['normal_left'])
+                sheet.write(row, 13, item['precio_unitario'], formats['normal_center'])
+                sheet.write(row, 14, item['monto_sin_igv'], formats['normal_center'])
+                sheet.write(row, 15, item['tipo_moneda'], formats['normal_center'])
+                sheet.write(row, 16, item['fecha_compra'], formats['date_format'])
+                sheet.write(row, 17, item['usuario_emisor'], formats['normal_center'])
                 row += 1
         else:
             sheet.write(row, 0, 'NO HAY DATOS / REPORTE EN CONSTRUCCION', )
@@ -112,11 +117,11 @@ class ReportPreorderProject(models.AbstractModel):
         standard_price = product.standard_price
         return standard_price if standard_price else 0
 
-    def get_data(self, id):
+    def get_data(self, from_date, to_date):
         query = """
             (SELECT
                 pp.name,
-                pp.date_order,
+                pp.date_order as date_order,
                 pp2.name as name_project,
                 po.name name_purchase_order,
                 po.id as id_purchase_order,
@@ -147,13 +152,14 @@ class ReportPreorderProject(models.AbstractModel):
                 LEFT JOIN product_template pt ON pt.id = pp3.product_tmpl_id
                 LEFT JOIN uom_uom uu ON pt.uom_id = uu.id
             WHERE
-                pp.project_id = %s)
+                pp.date_order >= '%s'
+                AND pp.date_order <= '%s')
 
             UNION ALL
 
             (SELECT
                 pp.name,
-                pp.date_order,
+                pp.date_order as date_order,
                 pp2.name as name_project,
                 NULL name_purchase_order,
                 NULL as id_purchase_order,
@@ -178,15 +184,16 @@ class ReportPreorderProject(models.AbstractModel):
                 LEFT JOIN product_template pt ON pt.id = pp3.product_tmpl_id
                 LEFT JOIN uom_uom uu ON pt.uom_id = uu.id
             WHERE
-                pp.project_id = %s
+                pp.date_order >= '%s'
+                AND pp.date_order <= '%s'
                 AND NOT EXISTS (
                     SELECT 1
                     FROM purchase_order po
                     INNER JOIN purchase_order_line pol ON pol.order_id = po.id
                     WHERE po.from_preorders = pp.name AND pol.product_id = ppl.product_id))
 
-            ORDER BY name_project desc;
-        """ % (id, id)
+            ORDER BY date_order ASC, name_project ASC;
+        """ % (from_date, to_date, from_date, to_date)
 
         log.info(query)
 
--- report_preorders_for_project/wizard/wizard_preorder_project.py
+++ report_preorders_for_project/wizard/wizard_preorder_project.py
@@ -10,7 +10,8 @@ class WizardPreorderProject(models.TransientModel):
     _name = 'wizard.preorder_for_project'
     _description = 'Reporte de pre-ordenes por proyecto'
 
-    project_id = fields.Many2one('project.project', string='Proyecto', required=True)
+    from_date = fields.Date(string='Desde', required=True)
+    to_date = fields.Date(string='Hasta', required=True)
     
     def generate_report(self):
         log.info('generate_report')
@@ -20,6 +21,7 @@ class WizardPreorderProject(models.TransientModel):
             'report_type': 'xlsx',
             'lines': self,
             'data': {
-                'project_id': self.project_id.id,
+                'from_date': self.from_date,
+                'to_date': self.to_date,
             },
         }
