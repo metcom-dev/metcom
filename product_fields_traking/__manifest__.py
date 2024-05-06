4fea2fc6817c5f22c6a87a4f330e50720ee5656b
Raul122-dev <victor.huacho@tecsup.edu.pe>
2024-05-06 14:07:52 -0500

    [IMP] product_fields_traking: modulo para agregar seguimiento a los campos de producto

---
+++ product_fields_traking/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Fields Traking",
+    'version': '16.0.0',
+    'category': 'Stock',
+    'author': 'Conflux',
+    'description': """
+        Agrega seguimiento a los campos de producto
+    """,
+    'depends': ["stock"],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
